74cddeba0a2eb4e1608e90f143b00f7f24dd1ed3
MysticBoy <mysticboy@live.com>
2022-12-29 01:43:51 +0800

    jiob

--- oschina2022/run.cmd
+++ oschina2022/run.cmd
@@ -1,6 +1,17 @@
-set objId =48907
+set objId=48907
 set g_user_id=7172
-set pow_batch=1000
+set pow_batch=100
 set oscid=EupA0S%%2BLlRps2qK%%2BvOCFLum1%%2BCv88Wm0L6LSTjShgc206yB6BHMqwvaw9Vj6DPdFreTNHljBVX%%2F%%2BbOC5hyTCAo4obVsdTgn91vvWi4sSZRbeTv6pN9afrAUBJScwv9pgQNQLFrKKiy3HyxoeAU9e6NoXVvaWBh95FgxptmOmVZA%%3D
 
-oschina2022.exe
+
+
+@echo off
+SET /A "index=1"
+SET /A "count=10000"
+:while
+if %index% leq %count% (
+ oschina2022.exe
+   SET /A "index=index + 1"
+   goto :while
+)
+ 
--- oschina2022/run.sh
+++ oschina2022/run.sh
@@ -1,5 +1,10 @@
-export  objId =48907
+#!/bin/bash
+export  objId=48907
 export  g_user_id=7172
 export  pow_batch=1000
 export  oscid=EupA0S%2BLlRps2qK%2BvOCFLum1%2BCv88Wm0L6LSTjShgc206yB6BHMqwvaw9Vj6DPdFreTNHljBVX%2F%2BbOC5hyTCAo4obVsdTgn91vvWi4sSZRbeTv6pN9afrAUBJScwv9pgQNQLFrKKiy3HyxoeAU9e6NoXVvaWBh95FgxptmOmVZA%3D
+
+for value in {1..10000}  
+do  
 ./oschina2022 
+done 
