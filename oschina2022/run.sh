c5985f483864c2eb04909bd9073a8519bb52226a
MysticBoy <mysticboy@live.com>
2022-12-29 02:05:21 +0800

    100

--- oschina2022/run.sh
+++ oschina2022/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export  objId=48907
 export  g_user_id=7172
-export  pow_batch=1000
+export  pow_batch=100
 export  oscid=EupA0S%2BLlRps2qK%2BvOCFLum1%2BCv88Wm0L6LSTjShgc206yB6BHMqwvaw9Vj6DPdFreTNHljBVX%2F%2BbOC5hyTCAo4obVsdTgn91vvWi4sSZRbeTv6pN9afrAUBJScwv9pgQNQLFrKKiy3HyxoeAU9e6NoXVvaWBh95FgxptmOmVZA%3D
 
 for value in {1..10000}  
