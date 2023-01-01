68c99b825dfbaeec65cf320f761bff42dbf9fe6e
MysticBoy <mysticboy@live.com>
2023-01-01 19:31:13 +0800

    避免分数统计 多线程时丢失

--- oschina2022/Program.cs
+++ oschina2022/Program.cs
@@ -12,13 +12,7 @@ namespace oschina2022
             var objId = Environment.ExpandEnvironmentVariables("%objId%");
             var g_user_id = Environment.ExpandEnvironmentVariables("%g_user_id%");
             var oscid = Environment.ExpandEnvironmentVariables("%oscid%").Trim('"');
-            var pow_batch = 5;
-            if (int.TryParse(Environment.ExpandEnvironmentVariables("%pow_batch%"), out pow_batch))
-            {
-                pow_batch = pow_batch < 5 ? 5 : pow_batch;
-            }
-            var pow_sig = 25;
-            Console.WriteLine($"项目ID:{objId}{Environment.NewLine}用户ID:{g_user_id}{Environment.NewLine}:登录信息:{oscid}{Environment.NewLine}并行计算{pow_batch}个线程，每线程{pow_sig}。{Environment.NewLine}");
+            Console.WriteLine($"项目ID:{objId}{Environment.NewLine}用户ID:{g_user_id}{Environment.NewLine}:登录信息:{oscid}{Environment.NewLine}。{Environment.NewLine}");
             var proxy = Environment.ExpandEnvironmentVariables("%https_proxy%");
             Console.WriteLine($"CPU数量:{Environment.ProcessorCount} 系统代理:{proxy}");
             CancellationTokenSource tokenSource = new CancellationTokenSource();
@@ -32,14 +26,17 @@ namespace oschina2022
             client.AddDefaultHeader("Content-Type", "application/json;charset=utf-8");
             client.AddDefaultHeader("User-Agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Mobile Safari/537.36");
             client.AddDefaultHeader("Cookie", $"oscid={oscid}");
-
-      
+            if (!string.IsNullOrEmpty(proxy) && proxy.StartsWith("https", StringComparison.OrdinalIgnoreCase))
+            {
+                client.Options.Proxy = new WebProxy(proxy);
+            }
+            var lstcheck = DateTime.MinValue;
             do
             {
                 var dt = DateTime.Now;
                 Console.WriteLine($"线程{Task.CurrentId}开始计算{dt.ToString("yyyy-MM-dd HH:mm.ss.ffff")}");
                 int _count = 0;
-                int _scores = 0;
+                 int _scores = 0;
                 ConcurrentBag<Oscresult> oscresults = new ConcurrentBag<Oscresult>();
                 Parallel.For(1, 26,  _ =>
                 {
@@ -47,18 +44,16 @@ namespace oschina2022
                     var osc = Pow(g_user_id, objId, out string sha1, out int scores, tokenSource.Token);
                     if (osc != null)
                     {
-                        oscresults.Add(osc);
-                        _count++;
-                        _scores++;
-                        Console.WriteLine($"CPU{Environment.ProcessId}线程{Thread.CurrentThread.ManagedThreadId}算得热度值{scores}，耗时{DateTime.Now.Subtract(dt2).TotalSeconds}");
+                        lock (oscresults)
+                        {
+                            oscresults.Add(osc);
+                            _count++;
+                            _scores += scores;
+                            Console.WriteLine($"线程{Thread.CurrentThread.ManagedThreadId}算得热度值{scores}，耗时{DateTime.Now.Subtract(dt2).TotalSeconds}");
+                        }
                     }
                 });
                 Console.WriteLine($"线程{Task.CurrentId}计算完成耗时{DateTime.Now.Subtract(dt).TotalSeconds}{Environment.NewLine}开始提交{DateTime.Now.ToString("yyyy-MM-dd HH:mm.ss.ffff")}{Environment.NewLine}");
-               
-                if (!string.IsNullOrEmpty(proxy) && proxy.StartsWith("https", StringComparison.OrdinalIgnoreCase))
-                {
-                    client.Options.Proxy = new WebProxy(proxy);
-                }
 
                var pow= Task.Run(() =>
                 {
@@ -69,18 +64,23 @@ namespace oschina2022
                     Console.WriteLine($"{result.StatusCode} {result.Content}");
                     Console.WriteLine($"线程{Task.CurrentId}提交完成,总数:{_count},总热度值:{_scores}.耗时{DateTime.Now.Subtract(dt1).TotalSeconds}");
                 });
-                var pow2= Task.Run(() =>
+
+                var pow2 = Task.Run(() =>
                 {
-                    var dt1 = DateTime.Now;
-                    RestRequest req = new RestRequest($"https://www.oschina.net/action/api/pow_integral?project={objId}", Method.Get);
-                    var rep = client.Execute(req, tokenSource.Token);
-                    Console.WriteLine($"最新热度值:{rep.StatusCode} {rep.Content}耗时{DateTime.Now.Subtract(dt1).TotalSeconds}");
+                    if (DateTime.Now.Subtract(lstcheck).TotalMinutes > 5)
+                    {
+                        lstcheck = DateTime.Now;
+                        var dt1 = DateTime.Now;
+                        RestRequest req = new RestRequest($"https://www.oschina.net/action/api/pow_integral?project={objId}", Method.Get);
+                        var rep = client.Execute(req, tokenSource.Token);
+                        Console.WriteLine($"最新热度值:{rep.StatusCode} {rep.Content}耗时{DateTime.Now.Subtract(dt1).TotalSeconds}");
+                    }
                 });
                 Task.WaitAll(pow, pow2);
             } while (!tokenSource.IsCancellationRequested);
         }
 
- 
+
         static string RandomString(int length)
         {
             const string characters = "abcdefghijklmnopqrstuvwxyz0123456789``!@#$%^&*()_+=-,./?><|\\}]{[";
