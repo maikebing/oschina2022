0e6960096d0297d833d8354f8c9d07d3f9991678
MysticBoy <mysticboy@live.com>
2023-01-04 16:11:03 +0800

    修改忽略想

--- oschina2022/Program.cs
+++ oschina2022/Program.cs
@@ -1,20 +1,29 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Security.Cryptography;
 using System.Text;
+using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Threading;
 using System.Threading.Tasks;
 
 RandomNumberGenerator random = RandomNumberGenerator.Create();
-var objId = Environment.ExpandEnvironmentVariables("%objId%");
-var g_user_id = Environment.ExpandEnvironmentVariables("%g_user_id%");
-var oscid = Environment.ExpandEnvironmentVariables("%oscid%").Trim('"');
-Console.WriteLine($"项目ID:{objId}{Environment.NewLine}用户ID:{g_user_id}{Environment.NewLine}:登录信息:{oscid}{Environment.NewLine}。{Environment.NewLine}");
-var proxy = Environment.ExpandEnvironmentVariables("%https_proxy%");
+var jd = JsonNode.Parse(System.IO.File.ReadAllText("config.json"));
+var objId = jd["project"]?.GetValue<string>();
+var proxy = jd["proxy"]?.GetValue<string>();
+List<(string g_user_id, string oscid)> _user = new List<(string g_user_id, string oscid)>();
+jd["users"].AsArray().ToList().ForEach(n =>
+{
+    var u = n["g_user_id"]?.GetValue<string>();
+    var osid = n["oscid"]?.GetValue<string>();
+    _user.Add((u, osid));
+});
+
+Console.WriteLine($"项目ID:{objId}{Environment.NewLine}用户信息共计{_user.Count}个{Environment.NewLine}");
 Console.WriteLine($"CPU数量:{Environment.ProcessorCount} 系统代理:{proxy}");
 CancellationTokenSource tokenSource = new();
 Console.CancelKeyPress += (object sender, ConsoleCancelEventArgs e) =>
@@ -34,80 +43,75 @@ client.Timeout = TimeSpan.FromMinutes(3);
 client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
 client.DefaultRequestHeaders.AcceptCharset.Add(new System.Net.Http.Headers.StringWithQualityHeaderValue("utf-8"));
 client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Mobile Safari/537.36");
-client.DefaultRequestHeaders.Add("Cookie", $"oscid={oscid}");
+
 var lstcheck = DateTime.MinValue;
+bool _is_full20w = false;
+var oscresults = new Queue<PowDto>();
+ReloadInfo(objId, client);
 do
 {
+    var user = _user[RandomNumberGenerator.GetInt32(0, _user.Count)];
     var dt = DateTime.Now;
     Console.WriteLine($"线程{Task.CurrentId}开始计算{dt:yyyy-MM-dd HH:mm.ss.ffff}");
     int _count = 0;
     int _scores = 0;
-    var oscresults = new ConcurrentBag<PowDto>();
-    Parallel.For(1, 31, _ =>
+
+    var _par = _is_full20w ? 100 : 30;
+
+    Parallel.For(0, _par, _ =>
     {
         var dt2 = DateTime.Now;
-        var osc = Pow(g_user_id, objId, out string sha1, out int scores, tokenSource.Token);
+        var osc = Pow(user.g_user_id, objId, out string sha1, out int scores, tokenSource.Token);
         if (osc != null)
         {
-            oscresults.Add(osc);
+            oscresults.Enqueue(osc);
             _count++;
             _scores += scores;
-            Console.WriteLine($"线程{Environment.CurrentManagedThreadId} SHA1:{sha1}  耗时{DateTime.Now.Subtract(dt2).TotalSeconds}");
+            Console.WriteLine($"线程{Environment.CurrentManagedThreadId} SHA1:{sha1} 获得{scores}热度值 耗时{DateTime.Now.Subtract(dt2).TotalSeconds}");
         }
     });
     Console.WriteLine($"线程{Task.CurrentId}计算完成耗时{DateTime.Now.Subtract(dt).TotalSeconds}{Environment.NewLine}开始提交{DateTime.Now:yyyy-MM-dd HH:mm.ss.ffff}{Environment.NewLine}");
-
-    Task.Run(async () =>
+    if (oscresults.Count > 25)
     {
-        try
+        Task.Run(async () =>
         {
-            var dt1 = DateTime.Now;
-            var rest = new HttpRequestMessage(HttpMethod.Post, "https://www.oschina.net/action/api/pow");
-            var array = new JsonArray();
-            foreach (var p in oscresults)
+            try
             {
-                array.Add(new JsonObject
+                var dt1 = DateTime.Now;
+                var rest = new HttpRequestMessage(HttpMethod.Post, "https://www.oschina.net/action/api/pow");
+                rest.Headers.Add("Cookie", $"oscid={user.oscid}");
+                var array = new JsonArray();
+                for (int i = 0; i < 30; i++)
                 {
+                    if (oscresults.TryDequeue(out var p))
+                    {
+                        array.Add(new JsonObject
+                    {
                     { "user", p.user },
                     { "token", p.token },
                     { "project", p.project },
                     { "counter", p.counter }
-                }.Root);
-            }
-            rest.Content = new StringContent(array.ToString());
-            var result = await client.SendAsync(rest);
-            var context = await result.Content?.ReadAsStringAsync();
-            var jo = JsonNode.Parse(context ?? "{}");
-            var _integral = jo["data"]?.AsArray().Sum(jn => jn["integral"].GetValue<long>());
-            Console.WriteLine($"计算期望热度{_scores}实际获得热度：{_integral} 服务器返回:{(_integral > 0 ? result.StatusCode.ToString() : context ?? result.ReasonPhrase)}耗时{DateTime.Now.Subtract(dt1).TotalSeconds} ");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"提交计算结果时遇到错误:{ex.Message}");
-        }
-    });
-
-    Task.Run(async () =>
-    {
-        if (DateTime.Now.Subtract(lstcheck).TotalMinutes > 5)
-        {
-            try
-            {
-                lstcheck = DateTime.Now;
-                var dt1 = DateTime.Now;
-                HttpRequestMessage rest = new(HttpMethod.Get, $"https://www.oschina.net/action/api/pow_integral?project={objId}");
-                var rep = await client.SendAsync(rest);
-                var context = await rep.Content?.ReadAsStringAsync();
+                    }.Root);
+                    }
+                }
+                rest.Content = new StringContent(array.ToString());
+                var result = await client.SendAsync(rest);
+                var context = await result.Content?.ReadAsStringAsync();
                 var jo = JsonNode.Parse(context ?? "{}");
-                var _integral = jo["data"]?["integral"]?.AsValue().GetValue<long>();
-                Console.WriteLine($"最新热度值:{_integral} 服务器返回:{(_integral > 0 ? rep.StatusCode.ToString() : context ?? rep.ReasonPhrase)} 耗时{DateTime.Now.Subtract(dt1).TotalSeconds}");
+                var _integral = jo["data"]?.AsArray().Sum(jn => jn["integral"].GetValue<long>());
+                Console.WriteLine($"计算期望热度{_scores}实际获得热度：{_integral} 服务器返回:{(_integral > 0 ? result.StatusCode.ToString() : context ?? result.ReasonPhrase)}耗时{DateTime.Now.Subtract(dt1).TotalSeconds} ");
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"查询最新热度时遇到异常{ex.Message}");
+                Console.WriteLine($"提交计算结果时遇到错误:{ex.Message}");
             }
-        }
-    });
+        });
+    }
+    else
+    {
+        Console.WriteLine($"计算的结果数大于25个时在提交");
+    }
+    ReloadInfo(objId, client);
 } while (!tokenSource.IsCancellationRequested);
 
 string RandomString(int length)
@@ -141,7 +145,7 @@ PowDto Pow(string oscid, string projectid, out string sha1, out int scores, Canc
             {
                 scores = 10;
             }
-            else if (sha1.StartsWith("00000"))
+            else if (!_is_full20w &&  sha1.StartsWith("00000"))
             {
                 scores = 1;
             }
@@ -156,6 +160,37 @@ PowDto Pow(string oscid, string projectid, out string sha1, out int scores, Canc
             counter++;
         }
     }
+}
+
+
+
+ void ReloadInfo(string objId, HttpClient client)
+{
+    Task.Run(async () =>
+    {
+        if (DateTime.Now.Subtract(lstcheck).TotalMinutes > 5)
+        {
+            try
+            {
+                lstcheck = DateTime.Now;
+                var dt1 = DateTime.Now;
+                HttpRequestMessage rest = new(HttpMethod.Get, $"https://www.oschina.net/action/api/pow_integral?project={objId}");
+                var rep = await client.SendAsync(rest);
+                var context = await rep.Content?.ReadAsStringAsync();
+                var jo = JsonNode.Parse(context ?? "{}");
+                var _integral = jo["data"]?["integral"]?.AsValue().GetValue<long>();
+                if (_integral > 200000)
+                {
+                    _is_full20w = true;
+                }
+                Console.WriteLine($"最新热度值:{_integral} 服务器返回:{(_integral > 0 ? rep.StatusCode.ToString() : context ?? rep.ReasonPhrase)} 耗时{DateTime.Now.Subtract(dt1).TotalSeconds}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"查询最新热度时遇到异常{ex.Message}");
+            }
+        }
+    });
 }
 
 public class PowDto
